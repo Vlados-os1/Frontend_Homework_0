3de27871de377baef1b5fe3f32fb4c252ce6c38b
vir$i <egrvrb@gmail.com>
2026-02-09 15:42:01 +0300

    mergeBy + tests + lint

---
+++ source/mergeBy.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * Объединяет два массива объектов по указанному ключу (например, 'id').
+ * * Логика слияния:
+ * 1. Если элемент с таким ключом уже есть в первом массиве, он обновляется данными из второго.
+ * 2. Если в обоих объектах есть свойство-массив, они объединяются (конкатенируются).
+ * 3. Если во втором массиве есть объект без указанного ключа, он игнорируется.
+ * @param {Array<Object>} arr1 - Исходный массив.
+ * @param {Array<Object>} arr2 - Массив с новыми данными (приоритетный).
+ * @param {string} key - Название поля-идентификатора (например, "id").
+ * @returns {Array<Object>} Новый объединенный массив.
+ */
+function mergeBy(arr1, arr2, key) {
+    const map = new Map();
+
+    arr1.forEach(item => {
+        const id = item[key];
+        if (id !== undefined) {
+            map.set(id, { ...item });
+        }
+    });
+
+    arr2.forEach(item => {
+        const id = item[key];
+
+        if (id === undefined) return;
+
+        if (map.has(id)) {
+            const existingItem = map.get(id);
+            const mergedItem = { ...existingItem };
+
+            Object.keys(item).forEach(prop => {
+                const val1 = existingItem[prop];
+                const val2 = item[prop];
+
+                if (Array.isArray(val1) && Array.isArray(val2)) {
+                    mergedItem[prop] = [...val1, ...val2];
+                } else {
+                    mergedItem[prop] = val2;
+                }
+            });
+
+            map.set(id, mergedItem);
+        } else {
+            map.set(id, { ...item });
+        }
+    });
+
+    return Array.from(map.values());
+}
